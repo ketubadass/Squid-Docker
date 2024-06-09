d519b2b8e4557cdf95ee2ee30bc6775970e99c33
ketubadass <ketubadass@gmail.com>
2024-06-09 08:08:35 -0300

    Update squid-proxy-docker.sh

--- squid-proxy-docker.sh
+++ squid-proxy-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p 8080:8080 squid-limbo /bin/bash -c "echo loading && apt-get update && apt-get upgrade -y && service squid start && htop"
+docker run -it -p 3128:8080 squid-limbo /bin/bash -c "echo loading && apt-get update && apt-get upgrade -y && service squid start && htop"
